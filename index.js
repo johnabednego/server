ea1e67a4ecc28bd256f452fc147e1d2b27afdfae
JOHN ABEDNEGO JILIMA <jajilima@st.ug.edu.gh>
2022-12-28 02:59:16 +0000

    server

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if(process.env.NODE_ENV==='production'){
 		res.sendFile(path.join(__dirname+"/build/index.html"))
 	})
 }
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 4001))
 
 const sanitizeString = (str) => {
 	return xss(str)
